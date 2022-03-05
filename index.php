2b41ff98b37850aa15fa326ee9ed15e741f0c665
hailm <hai.lemanh.1310@gmail.com>
2022-03-05 15:47:59 +0700

    update

--- index.php
+++ index.php
@@ -1,5 +1,72 @@
-<?php $data["title"] = "Trang chủ"; ?>
+<?php $data["title"] = "index"; ?>
 <?php $body = '' ?>
 <?php require "template-parts/layouts/header.php"; ?>
+<div class="uk-background-norepeat uk-background-cover" uk-height-viewport data-src="images/1x/bg1.png" uk-img>
+    <div class="uk-section-small">
+        <nav class="uk-navbar-container uk-navbar-transparent uk-margin" uk-navbar="mode: click">
+            <div class="uk-navbar-center">
+                <a href="" class="uk-navbar-item uk-logo"><img src="images/1x/logo.png" alt=""></a>
+            </div>
+        </nav>
+    </div>
+    <div class="uk-flex uk-flex-middle" uk-height-viewport="offset-top: true">
+        <div class="home__section uk-width-1-1 uk-section-small">
+            <div class="uk-container uk-container-small">
+                <div class="item__60">
+                    <div class="uk-child-width-auto uk-flex-middle uk-flex-center" uk-grid>
+                        <div>
+                            <a href=""><img src="images/trang-Dang-Ky---FPTU-but1.png" alt=""></a>
+                        </div>
+                        <div>
+                            <a href=""><img src="images/trang-Dang-Ky---FPTU-but2.png" alt=""></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="item__60">
+                    <div class="uk-child-width-1-2@m uk-flex-middle" uk-grid>
+                        <div>
+                            <div class="home__card uk-card uk-card-body uk-card-default uk-border-rounded">
+                                <form class="uk-form-stacked">
+                                    <fieldset class="uk-fieldset">
 
+                                        <legend class="uk-legend uk-text-uppercase home__card__title">đăng ký tư vấn</legend>
+
+                                        <div class="uk-margin">
+                                            <label class="uk-form-label home__card__label" for="form-stacked-text">Họ và tên</label>
+                                            <div class="uk-form-controls">
+                                                <input class="home__card__input uk-input uk-border-rounded" id="form-stacked-text" type="text" placeholder="">
+                                            </div>
+                                        </div>
+                                        <div class="uk-margin">
+                                            <label class="uk-form-label home__card__label" for="form-stacked-text">Số điện thoại</label>
+                                            <div class="uk-form-controls">
+                                                <input class="home__card__input uk-input uk-border-rounded" id="form-stacked-text" type="tel" placeholder="">
+                                            </div>
+                                        </div>
+                                        <div class="uk-margin">
+                                            <label class="uk-form-label home__card__label" for="form-stacked-text">Email</label>
+                                            <div class="uk-form-controls">
+                                                <input class="home__card__input uk-input uk-border-rounded" id="form-stacked-text" type="email" placeholder="">
+                                            </div>
+                                        </div>
+                                        <div class="uk-margin-top">
+                                            <label class="uk-form-label home__card__label" for="form-stacked-text">Tỉnh/ thành phố</label>
+                                            <div class="uk-form-controls">
+                                                <input class="home__card__input uk-input uk-border-rounded" id="form-stacked-text" type="text" placeholder="">
+                                            </div>
+                                        </div>
+
+                                    </fieldset>
+                                </form>
+                            </div>
+                        </div>
+                        <div>
+                            <img src="images/1x/img1.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <?php require "template-parts/layouts/footer.php"; ?>
---
+++ index1.php
@@ -0,0 +1,79 @@
+<?php $data["title"] = "index1"; ?>
+<?php $body = '' ?>
+<?php require "template-parts/layouts/header.php"; ?>
+<div class="uk-background-norepeat uk-background-cover" uk-height-viewport data-src="images/1x/bg1.png" uk-img>
+    <div class="uk-section-small">
+        <nav class="uk-navbar-container uk-navbar-transparent uk-margin" uk-navbar="mode: click">
+            <div class="uk-navbar-center">
+                <a href="" class="uk-navbar-item home__nav-item uk-logo"><img src="images/1x/logo.png" alt=""></a>
+                <div class="uk-navbar-item home__nav-item uk-visible@m">
+                    <div class="uk-text-center">
+                        <div class="home__txt1">PHIẾU ĐĂNG KÝ ĐẠI HỌC FPT 2022</div>
+                        <div class="home__txt2">HỆ ĐẠI HỌC CHÍNH QUY</div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
+    <div class="uk-flex uk-flex-middle" uk-height-viewport="offset-bottom: true;offset-top: true;">
+        <div class="home__section uk-width-1-1 uk-section-small">
+            <div class="uk-container uk-container-small">
+                <div class="home__card1 uk-card uk-card-default">
+                    <div>
+
+                    </div>
+                    <div class="home__card1__body uk-card-body">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="uk-section-small footer">
+        <div class="uk-container uk-container-large">
+            <div class="uk-child-width-1-5@m" uk-grid>
+                <div>
+                    <div class="footer__title">HÀ NỘI</div>
+                    <ul class="uk-list footer__list">
+                        <li>Khu Giáo dục và Đào tạo – Khu Công nghệ cao Hòa Lạc – Km29 Đại lộ Thăng Long, H. Thạch Thất, TP. Hà Nội</li>
+                        <li>024 7300 1866</li>
+                        <li>daihocfpt@fpt.edu.vn</li>
+                    </ul>
+                </div>
+                <div>
+                    <div class="footer__title">TP. HỒ CHÍ MINH</div>
+                    <ul class="uk-list footer__list">
+                        <li>Lô E2a-7, Đường D1 Khu Công nghệ cao, P. Long Thạnh Mỹ, TP. Thủ Đức, TP. Hồ Chí Minh</li>
+                        <li>024 7300 1866</li>
+                        <li>daihocfpt@fpt.edu.vn</li>
+                    </ul>
+                </div>
+                <div>
+                    <div class="footer__title">ĐÀ NẴNG</div>
+                    <ul class="uk-list footer__list">
+                        <li>Khu đô thị công nghệ FPT Đà Nẵng, P. Hoà Hải, Q. Ngũ Hành Sơn, TP. Đà Nẵng</li>
+                        <li>024 7300 1866</li>
+                        <li>daihocfpt@fpt.edu.vn</li>
+                    </ul>
+                </div>
+                <div>
+                    <div class="footer__title">CẦN THƠ</div>
+                    <ul class="uk-list footer__list">
+                        <li>Số 600 Đường Nguyễn Văn Cừ (nối dài), P. An Bình, Q. Ninh Kiều, TP. Cần Thơ</li>
+                        <li>024 7300 1866</li>
+                        <li>daihocfpt@fpt.edu.vn</li>
+                    </ul>
+                </div>
+                <div>
+                    <div class="footer__title">Bình Định</div>
+                    <ul class="uk-list footer__list">
+                        <li>Khu đô thị mới An Phú Thịnh, Phường Nhơn Bình & Phường Đống Đa, TP. Quy Nhơn, Bình Định</li>
+                        <li>024 7300 1866</li>
+                        <li>daihocfpt@fpt.edu.vn</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php require "template-parts/layouts/footer.php"; ?>
